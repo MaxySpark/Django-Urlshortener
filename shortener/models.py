48fb2a7110a54c611f79b5190d9d228bcfc05e8f
MaxySpark <devmaxyspark@gmail.com>
2018-07-31 18:20:19 +0530

    ep11

--- shortener/models.py
+++ shortener/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class ShortURL(models.Model):
     url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=15,unique=True)
 
     def __str__(self):
         return str(self.url)
