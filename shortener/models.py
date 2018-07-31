682db66f190ef10edce4a1e4e9a7bd0f6ff661ab
MaxySpark <devmaxyspark@gmail.com>
2018-07-31 23:53:40 +0530

    override model save

--- shortener/models.py
+++ shortener/models.py
@@ -1,13 +1,21 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
 
-
+def code_generator(size=6,chars=string.ascii_lowercase + string.digits) :
+    return ''.join(random.choice(chars) for _ in range(size))
 class ShortURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=15,unique=True)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        print("poop")
+        self.shortcode = code_generator()
+        super(ShortURL, self).save(*args,**kwargs)
 
     def __str__(self):
         return str(self.url)
