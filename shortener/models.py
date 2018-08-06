712142cde1842e1fbecea6e81d86ccabe21c4453
MaxySpark <devmaxyspark@gmail.com>
2018-08-07 00:19:45 +0530

    Objects Model Manager

--- shortener/models.py
+++ shortener/models.py
@@ -1,20 +1,36 @@
-import random
-import string
 from django.db import models
 
+from .utils import create_shortcode
 # Create your models here.
 
-def code_generator(size=6,chars=string.ascii_lowercase + string.digits) :
-    return ''.join(random.choice(chars) for _ in range(size))
+class  ShortURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        qs_main = super(ShortURLManager,self).all(*args,**kwargs)
+        qs = qs_main.filter(active=False)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = ShortURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            print(q.shortcode)
+            q.save()
+            new_codes += 1
+        return "New ShortCodes Made {i}".format(i=new_codes)
+
 class ShortURL(models.Model):
-    url = models.CharField(max_length=220, )
-    shortcode = models.CharField(max_length=15,unique=True)
-    updated = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    url         = models.CharField(max_length=220, )
+    shortcode   = models.CharField(max_length=15,unique=True, null=True, blank=True)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    active      = models.BooleanField(default=True)
+
+    objects = ShortURLManager()
 
     def save(self, *args, **kwargs):
-        print("poop")
-        self.shortcode = code_generator()
+        if self.shortcode is None or self.shortcode == "" :
+            self.shortcode = create_shortcode(self)
         super(ShortURL, self).save(*args,**kwargs)
 
     def __str__(self):
